25aaf128aabf9b2c1a2ecc01fc6f8b12d6d89d02
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2022-08-01 13:44:07 +0200

    Removed unused constants

--- src/jvmMain/kotlin/models/Students.kt
+++ src/jvmMain/kotlin/models/Students.kt
@@ -20,7 +20,6 @@ object StudentTable : Table("main") {
     val birthday = date("birthday")
     val date_last_exam = date("date_last_exam")
     val is_trainer = bool("is_trainer")
-    val sticker_units = integer("sticker_units")
     val sticker_animal = text("sticker_animal")
     val sticker_recieved = integer("sticker_recieved")
     val sticker_date_recieved = date("sticker_date_recieved")
@@ -38,7 +37,6 @@ data class Student(
     val birthday: LocalDate?,
     val date_last_exam: LocalDate?,
     val is_trainer: Boolean,
-    val sticker_units: Int,
     val sticker_animal: String?,
     val sticker_recieved: Int,
     val sticker_date_recieved: LocalDate?,
@@ -46,7 +44,6 @@ data class Student(
     val radioClicked: Boolean = false, // for sticker dialog (all radio buttons must be clicked before button activated)
     val stickerRecieved: Boolean = false, // for sticker dialog, if radio button is checked or not
     val sticker_show_again: Boolean = false, // for sticker dialog, if student is still missing stickers and the dialog should open again with this student
-    val sticker_old_unit: Int
 )
 
 data class Trainer(
@@ -70,12 +67,10 @@ fun loadStudents(): List<Student> {
                 birthday = it[StudentTable.birthday],
                 date_last_exam = it[StudentTable.date_last_exam],
                 is_trainer = it[StudentTable.is_trainer],
-                sticker_units = it[StudentTable.sticker_units],
                 sticker_animal = it[StudentTable.sticker_animal],
                 sticker_recieved = it[StudentTable.sticker_recieved],
                 sticker_date_recieved = it[StudentTable.sticker_date_recieved],
-                sticker_recieved_by = it[StudentTable.sticker_recieved_by],
-                sticker_old_unit = it[StudentTable.sticker_units]
+                sticker_recieved_by = it[StudentTable.sticker_recieved_by]
             )
         }
     }
@@ -108,7 +103,6 @@ fun editStudentSticker(student: Student) {
         StudentTable.update(where = { StudentTable.id eq student.id }) {
             it[StudentTable.sticker_recieved] = student.sticker_recieved
             it[StudentTable.sticker_recieved_by] = student.sticker_recieved_by!!
-            it[StudentTable.sticker_units] = student.sticker_units
             it[StudentTable.sticker_animal] = student.sticker_animal!!
             it[StudentTable.sticker_date_recieved] = LocalDate.now()
         }
