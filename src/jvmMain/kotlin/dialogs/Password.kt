730928a15471dd1046491aad46cc9ecae9c01697
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2022-09-19 17:06:27 +0200

    password will be checked when hitting ENTER

--- src/jvmMain/kotlin/dialogs/Password.kt
+++ src/jvmMain/kotlin/dialogs/Password.kt
@@ -8,12 +8,15 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun passwordPrompt(password: String, result: (pwCorrect: Boolean) -> Unit) {
 
@@ -32,14 +35,28 @@ fun passwordPrompt(password: String, result: (pwCorrect: Boolean) -> Unit) {
             },
             singleLine = true,
             isError = errorTextField,
-            modifier = Modifier.padding(bottom = 16.dp),
+            modifier = Modifier.padding(bottom = 16.dp).onKeyEvent { keyEvent ->
+                if (keyEvent.key != Key.Enter) return@onKeyEvent false
+                if (keyEvent.type == KeyEventType.KeyUp) {
+                    //TODO: Function this
+                    val passwordCorrect = passwordFieldVal == password // <- Password
+                    errorTextField = !passwordCorrect
+                    textViewText = if (passwordCorrect) textViewText else "Passwort falsch!"
+
+                    result(passwordCorrect)
+                    //
+                }
+                true
+            },
             onValueChange = { passwordFieldVal = it })
         Button(onClick = {
+            //TODO: Function this
             val passwordCorrect = passwordFieldVal == password // <- Password
             errorTextField = !passwordCorrect
             textViewText = if (passwordCorrect) textViewText else "Passwort falsch!"
 
             result(passwordCorrect)
+            //
         }) { Text("OK") }
     }
 
