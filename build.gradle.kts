3e6da8ca8c06173eeb6ec5512aa9a9000e59cb43
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2023-06-25 14:36:43 +0200

    changed kotlin multiplatform to jvm
    + updated some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
     id("org.jetbrains.compose")
 }
 
@@ -17,34 +17,20 @@ repositories {
     maven("https://jitpack.io")
 }
 
-plugins.withId("org.jetbrains.kotlin.multiplatform") {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-    }
-}
-
-kotlin {
-    jvm { withJava() }
-    sourceSets {
-        named("jvmMain") {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                // https://youtrack.jetbrains.com/issue/KTIJ-22262/Compose-IDE-False-positive-Cannot-access-class-androidxcomposeuigeometrySize-error#focus=Comments-27-6447983.0-0
-                implementation("org.jetbrains.compose.ui:ui-graphics-desktop:${extra["compose.version"] as String}")
-                implementation("org.jetbrains.compose.ui:ui-geometry-desktop:${extra["compose.version"] as String}")
-                implementation("org.jetbrains.compose.foundation:foundation-desktop:${extra["compose.version"] as String}")
-                //
-                implementation("org.slf4j:slf4j-nop:2.0.3")
-                implementation("org.jetbrains.exposed:exposed-core:${extra["exposed.version"] as String}")
-                implementation("org.jetbrains.exposed:exposed-jdbc:${extra["exposed.version"] as String}")
-                implementation("org.jetbrains.exposed:exposed-java-time:${extra["exposed.version"] as String}")
-                implementation("org.postgresql:postgresql:42.5.1")
-                implementation("org.apache.commons:commons-csv:1.9.0")
-                implementation("cc.ekblad:4koma:1.1.0")
-            }
-
-        }
-    }
+dependencies {
+    implementation(compose.desktop.currentOs)
+    // https://youtrack.jetbrains.com/issue/KTIJ-22262/Compose-IDE-False-positive-Cannot-access-class-androidxcomposeuigeometrySize-error#focus=Comments-27-6447983.0-0
+    implementation("org.jetbrains.compose.ui:ui-graphics-desktop:${project.extra["compose.version"] as String}")
+    implementation("org.jetbrains.compose.ui:ui-geometry-desktop:${project.extra["compose.version"] as String}")
+    implementation("org.jetbrains.compose.foundation:foundation-desktop:${project.extra["compose.version"] as String}")
+    //
+    implementation("org.slf4j:slf4j-nop:2.0.7")
+    implementation("org.jetbrains.exposed:exposed-core:${project.extra["exposed.version"] as String}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${project.extra["exposed.version"] as String}")
+    implementation("org.jetbrains.exposed:exposed-java-time:${project.extra["exposed.version"] as String}")
+    implementation("org.postgresql:postgresql:42.5.2")
+    implementation("org.apache.commons:commons-csv:1.10.0")
+    implementation("cc.ekblad:4koma:1.1.0")
 }
 
 compose.desktop {
